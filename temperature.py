15640628aba9eebc738ce9d1608531e15b928b52
camillejmora <cmor7802@uni.sydney.edu.au>
2023-03-19 16:04:47 +1100

    Updated legend to include the negative and positive indicators for "No Smoothing"

--- temperature.py
+++ temperature.py
@@ -1,6 +1,37 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+# import numpy as np
+# import sqlalchemy
+import sqlite3
+
+# Connect to the database (create it if it doesn't exist)
+conn = sqlite3.connect('database.db')
+
+# Read the data from the spreadsheet into a Pandas DataFrame
+data = pd.read_excel('data.xlsx')
+
+# Write the DataFrame to the database table 'data'
+data.to_sql('data', conn, if_exists='replace', index=False)
+
+# Close the database connection
+conn.close()
+
+# Open a new connection to the database
+conn = sqlite3.connect('database.db')
+
+# Add new data to the database
+new_data = pd.DataFrame({'X': [2024], 'Y': [0.8], 'Y2': [1.2]})
+new_data.to_sql('data', conn, if_exists='append', index=False)
+
+# Query the database for the updated data
+# database = pd.read_sql('SELECT * FROM data', conn)
+
+# Save the updated data to the Excel file
+updated_data = pd.read_sql('SELECT * FROM data', conn)
+updated_data.to_excel('data.xlsx', index=False)
+
+# Close the database connection
+conn.close()
 
 # Read the data from the spreadsheet into a Pandas DataFrame
 data = pd.read_excel('data.xlsx')
@@ -16,10 +47,17 @@ bars = plt.bar(x_axis, y_axis, width=5, color=['blue' if y < 0 else 'red' for y 
 line, = plt.plot(x_axis, y_axis2, color='black')
 
 # add a legend for Y1 that includes the colors for the both bars and the Y2 line
-plt.legend(handles=[bars[0], bars[-1], line], labels=['No Smoothing - negative', 'No Smoothing - positive', 'Y2'])
+plt.legend(handles=[bars[0], bars[-1], line], labels=['No Smoothing - negative', 'No Smoothing - positive', 'Smoothed'])
 
 # add a caption on the figure below the plot
-plt.figtext(.5, .0001, "Source: https://climate.nasa.gov/vital-signs/global-temperature/", horizontalalignment='center', fontsize=8)
+plt.figtext(.5, .0001, "Source: https://climate.nasa.gov/vital-signs/global-temperature/", horizontalalignment='left', fontsize=8)
+
+# Adjust the spacing between the subplots
+plt.subplots_adjust(bottom=0.2)
+
+# set figure size to make room for the legend
+fig = plt.gcf()
+fig.set_size_inches(10, 6)
 
 # plot the data
 plt.title("Global Land-Ocean Temperature Index")
@@ -29,4 +67,3 @@ plt.show()
 
 # save the plot as a png file
 plt.savefig('temperature.png')
-
