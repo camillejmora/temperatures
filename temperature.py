225b881c68ffa95313889b64d1b0516890f239fd
camillejmora <cmor7802@uni.sydney.edu.au>
2023-03-19 15:14:38 +1100

    Updated legend to include the negative and positive indicators for "No Smoothing"

--- temperature.py
+++ temperature.py
@@ -10,13 +10,13 @@ y_axis = data['Y']
 y_axis2 = data['Y2']
 
 # make the y values less than 0 the color blue and greater than 0 the color red
-plt.bar(x_axis, y_axis, width=5, color=['blue' if y < 0 else 'red' for y in y_axis])
+bars = plt.bar(x_axis, y_axis, width=5, color=['blue' if y < 0 else 'red' for y in y_axis])
 
 # plot the y2 values as a line
-plt.plot(x_axis, y_axis2, color='black')
+line, = plt.plot(x_axis, y_axis2, color='black')
 
 # add a legend for Y1 that includes the colors for the both bars and the Y2 line
-plt.legend(['Lowess 5', 'No smoothing', 'Y1', 'Y2'])
+plt.legend(handles=[bars[0], bars[-1], line], labels=['No Smoothing - negative', 'No Smoothing - positive', 'Y2'])
 
 # add a caption on the figure below the plot
 plt.figtext(.5, .0001, "Source: https://climate.nasa.gov/vital-signs/global-temperature/", horizontalalignment='center', fontsize=8)
@@ -26,4 +26,7 @@ plt.title("Global Land-Ocean Temperature Index")
 plt.xlabel("Temperature")
 plt.ylabel("Years")
 plt.show()
+
+# save the plot as a png file
+plt.savefig('temperature.png')
 
